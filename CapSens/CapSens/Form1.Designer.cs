4b3442c4072c16835b944e74794ced1920e9b165
microchipdeveloper <32633404+microchipdeveloper@users.noreply.github.com>
2021-05-06 07:24:16 +0300

    1

---
+++ CapSens/CapSens/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace CapSens
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.cbPort = new System.Windows.Forms.ComboBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.tbStatus = new System.Windows.Forms.TextBox();
+            this.devAdr = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.devAdr)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // cbPort
+            // 
+            this.cbPort.FormattingEnabled = true;
+            this.cbPort.Location = new System.Drawing.Point(13, 44);
+            this.cbPort.Name = "cbPort";
+            this.cbPort.Size = new System.Drawing.Size(121, 21);
+            this.cbPort.TabIndex = 0;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.BackColor = System.Drawing.Color.LightGray;
+            this.btnOpen.Location = new System.Drawing.Point(12, 117);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Открыть";
+            this.btnOpen.UseVisualStyleBackColor = false;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // tbStatus
+            // 
+            this.tbStatus.Location = new System.Drawing.Point(12, 91);
+            this.tbStatus.Name = "tbStatus";
+            this.tbStatus.Size = new System.Drawing.Size(243, 20);
+            this.tbStatus.TabIndex = 3;
+            // 
+            // devAdr
+            // 
+            this.devAdr.Location = new System.Drawing.Point(12, 68);
+            this.devAdr.Name = "devAdr";
+            this.devAdr.Size = new System.Drawing.Size(120, 20);
+            this.devAdr.TabIndex = 4;
+            this.devAdr.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(405, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(27, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(386, 133);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(27, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(405, 180);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(27, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(502, 89);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(27, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "6";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(519, 133);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(27, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(502, 180);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(27, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "4";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(451, 133);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(27, 23);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.devAdr);
+            this.Controls.Add(this.tbStatus);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.cbPort);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.devAdr)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox cbPort;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox tbStatus;
+        private System.Windows.Forms.NumericUpDown devAdr;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ CapSens/CapSens/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace CapSens
+{
+    public partial class Form1 : Form
+    {
+        public byte[] devStatus = new byte[20];
+        public byte[] cmd = new byte[64];
+        public byte[] indataPort1 = new byte[255];
+        byte rxdLen;
+        private bool portNewData = false;
+        long ElapsedTime;
+        //----------------------------------------------------------------------------------------
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbPort.Items.AddRange(items: SerialPort.GetPortNames());
+            if (cbPort.Items.Count > 0) cbPort.SelectedIndex = 0;
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    portCloseAtrbt();
+                }
+                else
+                {   cbPort.Text = "COM3";
+                    serialPort1.PortName = cbPort.Text;
+                    serialPort1.Open();
+                    btnOpen.Text = "Закрыть";
+                    btnOpen.BackColor = Color.Green;
+                    timer1.Start();
+                }
+            }
+            catch (Exception err)
+            {
+                portCloseAtrbt();
+                MessageBox.Show(err.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        // Конвертеры ---------------------------------------------------------------------------------------
+        private string AsciiToString(byte[] indata, int ofset, byte len)   // Конвертер ASCII в строку
+        {
+            String retStr = "";
+            char x;
+            while (indata[ofset] != 0 && len > 0)
+            {
+                x = (char)indata[ofset];
+                if (x > 0xBF)
+                {
+                    x += (char)0x0350;
+                }
+                retStr += x;
+                ofset++;
+                len--;
+            }
+            return retStr;
+        }
+        private byte ChrToASCII(char symb)                                      // Конвертер Char -> ASCII
+        {
+            byte res;
+            if (symb >= 'А')
+            {
+                symb -= 'А';
+                symb += (char)0xC0;
+            }
+            res = (byte)symb;
+            return res;
+        }
+        private string HexToStr(byte data)                                      // Конвертер HEX в 2 символьную строку + пробел
+        {
+            var a = data;
+            char ch = '0';
+            a >>= 4;
+            if (a > 9)
+            {
+                a += 7;
+                while (a-- > 0) ch++;
+            }
+            else ch += (char)a;
+            string str = ch.ToString();
+            data &= 0x0F;
+            ch = '0';
+            if (data > 9)
+            {
+                data += 7;
+                while (data-- > 0) ch++;
+            }
+            else ch += (char)data;
+            str += ch.ToString();
+            return str + ' ';
+        }
+        //-----------------------------------------------------------------------------------------
+        private byte calcCRC8(byte[] buf, byte offset, byte cnt)
+        {
+            byte crc = 0, i = offset;
+            while (i < cnt)
+            {
+                crc += buf[i++];
+            }
+            return crc;
+        }
+        public byte portal_1(byte[] sendBuf, byte len, int wait)
+        {
+            sendBuf[len] = calcCRC8(sendBuf, 0, len);
+            rxdLen = 0;
+            try
+            {
+                serialPort1.Write(sendBuf, 0, ++len);
+            }
+            catch (Exception err)
+            {
+                portCloseAtrbt();
+                MessageBox.Show(err.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            Thread.Sleep(wait);
+            if (rxdLen > 3 && sendBuf[1] + 0x80 == indataPort1[len + 1])
+            {
+                rxdLen--;
+                if (calcCRC8(indataPort1, len, rxdLen) == indataPort1[rxdLen])
+                {
+                    //serialPort1.Write(indataPort1, len, rxdLen);
+                    return indataPort1[len + 1];            // Возвращает код функции
+                }
+            }
+            return 0;
+        }
+        public byte getStat(byte adr)
+        {
+            cmd[0] = adr;
+            cmd[1] = 0x77;
+            var tmout = 100;
+            if (serialPort1.BaudRate < 4800) tmout = 250;
+            if (portal_1(cmd, 2, tmout) == 0xF7)
+            {
+                return ParseStatus(3);
+            }
+            return 0;
+        }
+        private byte ParseStatus(byte ofset)
+        {
+            if (rxdLen > 250) rxdLen = 0;         // иногда вылетает исключение
+            if (calcCRC8(indataPort1, ofset, rxdLen) == indataPort1[rxdLen])
+            {
+                if (indataPort1[ofset + 1] == 0xF7 || indataPort1[ofset + 1] == 0xFF)//
+                {
+                    for (var i = 0; i < rxdLen; i++)
+                    {
+                        devStatus[i] = indataPort1[i + 2 + ofset];
+                    }
+                    return indataPort1[ofset + 2];              // Вернуть количество байт статусного ответа
+                }
+            }
+            /*      else if (indataPort1[ofset + 1] == 0xF6)
+                  {
+                      if (calcCRC8(indataPort1, ofset, 3) == indataPort1[3])
+                      {
+                          devStatus[1] = indataPort1[ofset + 2];
+                          KpiStatInfo();
+                      }
+                  }*/
+            return 0;
+        }
+        private void portCloseAtrbt()
+        {
+            serialPort1.Close();
+            timer1.Stop();
+            btnOpen.Text = "Открыть";
+            btnOpen.BackColor = Color.LightGray;
+            tbStatus.BackColor = Color.White;
+        }
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            int len;
+            try
+            {
+                portNewData = true;
+                len = serialPort1.BytesToRead;
+                serialPort1.Read(indataPort1, rxdLen, len);
+                rxdLen += (byte)len;
+                this.Invoke(new EventHandler(RXProcess));
+            }
+            catch (Exception err)
+            {
+                portCloseAtrbt();
+                MessageBox.Show(err.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void RXProcess(object sender, EventArgs e)
+        {
+            ElapsedTime = Stopwatch.GetTimestamp();
+                //       ElapsedTime *= ((long)1.0 / Stopwatch.Frequency);   //  - Приведение тиков к реальному времени в секундах
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!portNewData)
+            {
+                while ((Stopwatch.GetTimestamp() - ElapsedTime) < 25000)
+                {
+                    // пустой цикл ожидания
+                }
+                if (getStat((byte)devAdr.Value) != 0)
+                {
+                    tbStatus.BackColor = Color.LimeGreen;
+                }
+                else tbStatus.BackColor = Color.White;
+            }
+            else
+            {
+                portNewData = false;
+                rxdLen--;
+                ParseStatus(3);
+            }
+            tbStatus.Text = "";
+            var i = 1;
+            var x = devStatus[0];
+            while (x-- > 0 && x < devStatus.Length)
+            {
+                tbStatus.Text += HexToStr(devStatus[i++]);
+            }
+
+        }
+    }
+}
